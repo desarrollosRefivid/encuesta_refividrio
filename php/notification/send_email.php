77f1cf04912f7d18a361ac0ad058fd7b36da32f6
marcos-moreno <2517160124marcosgmg@gmail.com>
2020-09-17 17:56:15 -0500

    notifications

--- php/notification/send_email.php
+++ php/notification/send_email.php
@@ -1,17 +1,22 @@
 <?php
 
-$msg = $_GET['msg'];
-$para = $_GET['correo'];
-$title = $_GET['title'];
+try {
+    $msg = $_POST['msg'];
+    $para = $_POST['correo'];
+    $title = $_POST['title'];
 
-if (isset($msg) && isset($para)) {
-    $message = "<p>$msg</p>";
-    $to_email = $para;
-    $subject = $title;
-    $headers[] = 'MIME-Version: 1.0';
-    $headers[] = 'Content-type: text/html; charset=UTF-8';
-    $headers[] = 'From: Encuestas Refividrio.';
+    if (isset($msg) && isset($para)) {
+        $message = "<p>$msg</p>";
+        $to_email = $para;
+        $subject = $title;
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=UTF-8';
+        $headers[] = 'From: Encuestas Refividrio.';
 
-    mail($to_email, $subject, $message, implode("\r\n", $headers));
+        mail($to_email, $subject, $message, implode("\r\n", $headers));
+    }
+} catch (\Throwable $th) {
+   echo $th;
 }
+
 
